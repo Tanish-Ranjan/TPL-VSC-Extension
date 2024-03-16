01dc8d67a52a79ab3d074fbb344d6d597f109e54
Tanish-Ranjan <tanishranjan4@gmail.com>
2024-03-16 05:32:26 +0530

    Added menu commands for TPL Run and Debug instead of overriding the debug command for all.

--- extension.js
+++ extension.js
@@ -5,47 +5,57 @@ let activeTPLTerminal = null;
 
 function activate(context) {
 
-	const runButton = async () => {
-		const interpreterPath = vscode.workspace.getConfiguration().get('tpl.interpreterPath');
-		if (!interpreterPath) {
-			vscode.window.showErrorMessage("Please select the TPL interpreter path first!");
-			return
-		}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('tpl.runEditorContents', async (resource) => {
 
-		const activeTextEditor = vscode.window.activeTextEditor;
-		if (!activeTextEditor) {
-			return;
-		}
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
 
-		const scriptPath = activeTextEditor.document.uri.fsPath;
-		const command = 'java -jar ' + interpreterPath + ' ' + scriptPath;
+			if (targetResource) {
+				
+				const scriptPath = targetResource.fsPath;
+				const interpreterPath = vscode.workspace.getConfiguration().get('tpl.interpreterPath');
 
-		if (activeTPLTerminal) {
-
-			if (!activeTPLTerminal.exitStatus) {
-
-				try {
-					await activeTPLTerminal.show();
-					activeTPLTerminal.sendText(command);
-					return;
-				} catch (e) {
-					console.error("Error focusing or sending command to existing TPL terminal: " + e);
+				if (!interpreterPath) {
+					vscode.window.showErrorMessage("Please select the TPL interpreter path first!");
+					return
 				}
+
+				const command = 'java -jar ' + interpreterPath + ' ' + scriptPath;
+	
+				if (activeTPLTerminal) {
+	
+					if (!activeTPLTerminal.exitStatus) {
+	
+						try {
+							await activeTPLTerminal.show();
+							activeTPLTerminal.sendText(command);
+							return;
+						} catch (e) {
+							console.error("Error focusing or sending command to existing TPL terminal: " + e);
+						}
+	
+					}
+	
+				}
+	
+				activeTPLTerminal = createAndShowTPLTerminal(command);
 
 			}
 
-		}
+		}),
+		vscode.commands.registerCommand('tpl.debugEditorContents', async (resource) => {
 
-		activeTPLTerminal = createAndShowTPLTerminal(command);
+			vscode.window.showWarningMessage("TPL Debugging will be added soon!");
 
-	};
+		}),
+		vscode.commands.registerCommand('tpl.selectInterpreter', () => {
+			selectInterpreter();
+		})
+	);
 
-	let selectInterpreterCmd = () => {
-		selectInterpreter();
-	};
-
-	context.subscriptions.push(vscode.commands.registerCommand('workbench.action.debug.run', runButton));
-	context.subscriptions.push(vscode.commands.registerCommand('tpl.selectInterpreter', selectInterpreterCmd));
 }
 
 function createAndShowTPLTerminal(command) {
